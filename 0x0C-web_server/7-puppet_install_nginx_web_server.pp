5ec1fa97f7ee972af2034a2a0e259c12616eaff7
Wise2525 <ayesiganobert@gmail.com>
2022-01-05 07:26:53 -0800

    webserver

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
